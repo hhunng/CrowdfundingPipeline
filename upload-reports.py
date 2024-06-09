d8b9db6c693d1c5f2be387d15451dd02c5d6bd51
hunglp <phihung21522115@gmail.com>
2024-06-09 11:04:23 +0700

    spin up the pipeline

---
+++ upload-reports.py
@@ -0,0 +1,39 @@
+import requests
+import sys
+
+file_name = sys.argv[1]
+token = sys.argv[2]
+scan_type = ''
+
+if file_name == 'gitleaks.json':
+    scan_type = 'Gitleaks Scan'
+elif file_name == 'dependency-check-report.sarif':
+    scan_type = 'SARIF'
+elif file_name == 'semgrep.json':
+    scan_type = 'Semgrep JSON Report'
+
+
+headers = {
+    'Authorization': f'Token {token}'
+}
+
+url = 'https://demo.defectdojo.org/api/v2/import-scan/'
+
+data = {
+    'active': True,
+    'verified': True,
+    'scan_type': scan_type,
+    'minimum_severity': 'Low',
+    'engagement': 14
+}
+
+files = {
+    'file': open(file_name, 'rb')
+}
+
+response = requests.post(url, headers=headers, data=data, files=files)
+
+if response.status_code == 201:
+    print('Scan results imported successfully')
+else:
+    print(f'Failed to import scan results: {response.content}')
